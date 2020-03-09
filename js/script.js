9c9c9f4aa3dd6ac60c5da4c461cb2661b02a7593
Markiian050 <markiayn120@gmail.com>
2020-03-09 03:21:08 +0200

    іі

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-$(document).ready(function() {
-	$('.popup-with-form').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#name',
+function initMap() {
+	var pos = {lat:49.868311,lng: 24.047712};
+	var opt={
+		center:pos,
+		zoom: 16
+		
+	}
+	var map = new google.maps.Map(document.getElementById("map"),opt);
+	var marker = new google.maps.Marker({
+		position:pos,
+		map: map,
+		title: 'MSLfurniture'
 
-		// When elemened is focused, some mobile browsers in some cases zoom in
-		// It looks not nice, so we disable it:
-		callbacks: {
-			beforeOpen: function() {
-				if($(window).width() < 700) {
-					this.st.focus = false;
-				} else {
-					this.st.focus = '#name';
-				}
-			}
-		}
 	});
-});
+	};
+
+
