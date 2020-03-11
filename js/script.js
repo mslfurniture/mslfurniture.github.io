8cbc9f41ca85dbff54cc2ac8bdbde0a40a886e7e
Markiian050 <markiayn120@gmail.com>
2020-03-12 00:29:15 +0200

    dssda

--- js/script.js
+++ js/script.js
@@ -25,7 +25,6 @@ function initMap() {
 			}).done(function() {
 				$(this).find("input").val("");
 				alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-				$("#form").trigger("reset");
 			});
 			return false;
 		});
