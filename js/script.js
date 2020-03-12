56406400a57e0ed2c798e8f8251f269b76bdc598
Markiian050 <markiayn120@gmail.com>
2020-03-13 00:14:51 +0200

    s

--- js/script.js
+++ js/script.js
@@ -13,25 +13,3 @@ function initMap() {
 
 	});
 	};
-
-	//E-mail Ajax Send
-	$(document).ready(function() {
-
-		//E-mail Ajax Send
-		$("#form").submit(function() { //Change
-			var th = $(this);
-			$.ajax({
-				type: "POST",
-				url: "mail.php", //Change
-				data: th.serialize()
-			}).done(function() {
-				alert("Thank you!");
-				setTimeout(function() {
-					// Done Functions
-					th.trigger("reset");
-				}, 1000);
-			});
-			return false;
-		});
-	
-	});
