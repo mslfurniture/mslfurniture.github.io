6fcb37007de0be680aeca3b0244d850681694b6e
Markiian050 <markiayn120@gmail.com>
2020-03-13 16:25:00 +0200

    ghf

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function initMap() {
 		function success() {
 		  form.reset();
 		  button.style = "display: none ";
-		  status.innerHTML = "Thanks!";
+		  status.innerHTML =alert("Вашу заявку надіслано. Чекайте відповіді") ;
 		}
 	
 		function error() {
