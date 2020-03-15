c2d07a869153d195b46731a0570ee3136bb2760d
Markiian050 <markiayn120@gmail.com>
2020-03-15 21:54:26 +0200

    шгрлш

--- js/script.js
+++ js/script.js
@@ -23,18 +23,42 @@ function initMap() {
 		var form = document.getElementById("my-form");
 		var button = document.getElementById("my-form-button");
 		var status = document.getElementById("my-form-status");
-	
+		var button_head = document.getElementById("button");+
+
+		button.addEventListener('click',function(){
+			function success() {
+				form.reset();
+				button.style = "display: none ";
+				Swal.fire({
+					position: 'top-end',
+					icon: 'success',
+					title: 'Дякуємо за Ваше повідомлення. Воно було відправлено.',
+					showConfirmButton: false,
+					timer: 1500
+				  })
+			}
 		// Success and Error functions for after the form is submitted
 		button.addEventListener('click',function(){
 			function success() {
 				form.reset();
 				button.style = "display: none ";
-				swal("Заявка успішно відправлена!", "Чекайте відповіді", "success");
+				Swal.fire({
+					position: 'top-end',
+					icon: 'success',
+					title: 'Дякуємо за Ваше повідомлення. Воно було відправлено.',
+					showConfirmButton: false,
+					timer: 1500
+				  })
 			  }
 		  
 			  function error() {
-				swal("Ошибка!", "Перезагрузіть сторінку і попробуйте знову", "error");
-			  }
+				Swal.fire({
+					position: 'top-end',
+					icon: 'success',
+					title: 'Перезагрузіть сторінку і попробуйте знову.',
+					showConfirmButton: false,
+					timer: 2500
+				  })			  }
 		  
 			  // handle the form submission event
 		  
@@ -64,9 +88,4 @@ function initMap() {
 		};
 		xhr.send(data);
 	  }
-
-
-
-$(document).ready(function(){
-	$("#phone").mask("(999) 999-9999");
-});
+	});
