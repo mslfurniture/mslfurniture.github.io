cf115e5dbf9c6da70e30157a6aec3e98c73baab4
Markiian050 <markiayn120@gmail.com>
2020-03-17 13:25:38 +0200

    as

--- script.js
+++ script.js
@@ -76,3 +76,10 @@ function initMap() {
 		xhr.send(data);
 		}
 	});
+	function slowScroll (id) {
+		var offset = 0;
+		$('html, body').animate ({
+		  scrollTop: $(id).offset ().top - offset
+		}, 500);
+		return false;
+	  }
